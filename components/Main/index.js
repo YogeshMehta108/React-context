35b864676af49dcb5f54a29dded8bc96a94d7681
unknown <yogeshmehta108>
2022-08-31 12:51:55 +0530

    adding new text

--- components/Main/index.js
+++ components/Main/index.js
@@ -3,6 +3,6 @@ import { usePlaceContext } from "../../context/Places";
 import styles from "./Main.module.css";
 
 export default function Main() {
-  const { places } = usePlaceContext();
+  const { places } = usePlaceContext(); //These are the Places 
   return <div className={styles.place_row}>{JSON.stringify(places)}</div>;
 }
