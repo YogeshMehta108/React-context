a06dceb5eb8795b3a3559c61231f0830b2a7e34f
YogeshMehta108 <yogeshprivate108@gmail.com>
2022-08-31 12:57:51 +0530

    removing text

--- components/Main/index.js
+++ components/Main/index.js
@@ -3,6 +3,6 @@ import { usePlaceContext } from "../../context/Places";
 import styles from "./Main.module.css";
 
 export default function Main() {
-  const { places } = usePlaceContext(); //These are the Places 
+  const { places } = usePlaceContext();
   return <div className={styles.place_row}>{JSON.stringify(places)}</div>;
 }
